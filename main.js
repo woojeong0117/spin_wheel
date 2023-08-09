f2883c85ba7e75b18b099e69aba64832f2d42fdc
정민호 <jeongminho@jeongminhoui-MacBookAir.local>
2023-08-09 23:04:43 +0900

    수정

--- main.js
+++ main.js
@@ -3,34 +3,43 @@ const wheel = document.querySelector(".wheel");
 const modal = document.querySelector(".modal_wrap");
 const btnClose = document.querySelector(".close");
 const text = document.querySelector(".modal > p");
+const title = document.querySelector('.modal > h1')
 let isOn = false;
 let rPos = 0;
-let rotationSpeed = 10; // Initial rotation speed
+let rotationSpeed = 0; // Initial rotation speed
 let animationFrameId;
 
 function rotateWheel() {
   rPos += rotationSpeed;
   wheel.style.transform = `rotate(${rPos}deg)`;
   animationFrameId = requestAnimationFrame(rotateWheel);
-
+  
   if (rPos > 360) {
     rPos -= 360;
   }
-
+  
   // Decrease rotation speed gradually when stopping
-  if (!isOn && rotationSpeed > 0.5) {
+  if (!isOn && rotationSpeed > 0.1) {
     rotationSpeed -= 0.1; // Adjust the decrement value for desired slowdown rate
-  } else if (!isOn && rotationSpeed <= 0.5) {
+  } else if (!isOn && rotationSpeed <= 0.1) {
     stop();
     setTimeout(() => {
       modal.classList.add("on");
     }, 1000);
+    
+    // Adjust rPos to the nearest 10's multiple
+    rPos = Math.floor(rPos / 10) * 10;
+    wheel.style.transform = `rotate(${rPos}deg)`;
+    console.log(rPos)
+
+
+    // Assign values based on rPos ranges
     if (rPos >= 0 && rPos <= 40) {
       text.innerText = "100점입니다😍";
     } else if (rPos > 40 && rPos <= 130) {
       text.innerText = "400점입니다😍";
     } else if (rPos > 130 && rPos <= 220) {
-      text.innerText = "300점입니다😍";
+      text.innerText = "꽝 🤪";
     } else if (rPos > 220 && rPos <= 310) {
       text.innerText = "200점입니다😍";
     } else if (rPos > 310) {
